d8be5b46fd0bb878305f112cef45e53f5508e767
Brandon Gannicott <brandon.gannicott@gmail.com>
2021-12-07 20:00:48 -0800

    add circe

--- build.sbt
+++ build.sbt
@@ -14,3 +14,11 @@ libraryDependencies ++= Seq(
   "dev.zio" %% "zio" % zioVersion,
   "io.d11" %% "zhttp" % "1.0.0.0-RC17"
 )
+
+val circeVersion = "0.14.1"
+
+libraryDependencies ++= Seq(
+  "io.circe" %% "circe-core",
+  "io.circe" %% "circe-generic",
+  "io.circe" %% "circe-parser"
+).map(_ % circeVersion)
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,14 +1,19 @@
 package com.ciphertrace
 
+import io.circe.syntax._
+
 import zio._
+import zio.console._
 import zhttp.http._
 import zhttp.service.Server
 
 object Main extends App {
+  val port = 8090
   val app = Http.collect[Request] {
-    case Method.GET -> Root / "text" => Response.text("Hello World!")
+    case Method.GET -> Root / "text" => Response.text("Hello, World")
+    case Method.GET -> Root / "json" => Response.jsonString(Seq("Hello", "World").asJson.spaces4)
   }
 
   override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] =
-    Server.start(8090, app).exitCode
+    (putStrLn(s"Server booting on $port!") <* Server.start(port, app)).exitCode
 }
